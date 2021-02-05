f1f0d2fa9222cc80c96f54f8069ed89549b8f34e
fand <fand@itu.dk>
2021-02-05 15:01:12 +0100

    Working around result-error handling

--- src/datasets.rs
+++ src/datasets.rs
@@ -1,53 +1,48 @@
 
-pub fn get_dataset() -> hdf5::Result<()> {
+pub fn get_dataset(filename: &str) -> hdf5::Result<()> {
     // so that libhdf5 doesn't print errors to stdout
     let _e = hdf5::silence_errors();
     {
-        let file = hdf5::File::open("../../datasets/glove-100-angular.hdf5")?;
-
-        
-        println!("HDF5 member_names: {:?}", file.member_names()?);
-        for x in file.member_names()? {
-            println!("{:?}", x)
-        }
-        
-        // println!("HDF5 size: {:?}", file.size());
-        // println!("HDF5 access_plist: {:?}", file.access_plist());
-        // println!("HDF5 create_plist: {:?}", file.create_plist());
-
+        let file = hdf5::File::open(filename)?;
         let train_dataset = file.dataset("train")?;
-        let train:ndarray::Array2::<f32> = train_dataset.read_2d::<f32>()?;
-        println!("train_dataset shape {:?}", train_dataset.shape());
-        println!("Vector length {:?}", train_dataset.shape()[1]);
-        println!("{:?}", train);
-        println!("Array length {:?}", train.shape()[0]);
-        println!("{}", train[[0,0]]);
-        // for i in 0..train.shape()[0] {
-        //     println!("{}: {:?}", i, train[[i,0]]);
-        // }
-            
-        // for x in train.genrows() {
-        //     println!("{:?}", x);
-        // }
-
-        // for (i, row) in train.iter().enumerate() {
-        //     for (y, col) in train.iter().enumerate() {
-        //         println!("{}", y);
-        //     }
-        // }
-
-        // let test_queries = file.dataset("test")?;
-        // println!("test_queries shape {:?}", test_queries.shape());
-        // println!("{:?}", test_queries.read_2d::<f32>()?);
-
-        // let neighbors = file.dataset("neighbors")?;
-        // println!("neighbors shape {:?}", neighbors.shape());
-        // println!("{:?}", neighbors.read_2d::<i32>()?);
-
-        // let distance = file.dataset("distance")?;
-        // println!("distance shape {:?}", distance.shape());
-        // println!("{:?}", distance.read_2d::<f32>()?);
     }
-
     Ok(())
+}
+
+enum HDF5_Dataset {
+    DISTANCE,
+    NEIGHBORS,
+    TRAIN,
+    TEST
+}
+
+pub fn get_data_array(filename: &str, dataset: &str) -> hdf5::Result<ndarray::Array2::<f32>> {
+    // so that libhdf5 doesn't print errors to stdout
+    let _e = hdf5::silence_errors();
+    let file = hdf5::File::open(filename)?;
+
+    println!("Loading: {} from {}", dataset, filename);
+    println!("Datasets in file: {:?}", file.member_names()?);
+    
+    let hdf5_data = file.dataset(dataset)?;
+    let mut res = ndarray::Array2::<f32>::zeros((0,0));
+    if dataset == "train" || dataset == "test" || dataset == "distances"  {
+        let data_2d_float:ndarray::Array2::<f32> = hdf5_data.read_2d::<f32>()?;
+        res = data_2d_float;
+        println!("dataset shape: {:?}", res.shape());
+    } else if dataset == "neighbors" {
+        let data_2d_int:ndarray::Array2::<i32> = hdf5_data.read_2d::<i32>()?;
+        println!("dataset shape: {:?}", data_2d_int.shape());
+    } else {
+        println!("ERROR: WRONG DATASET CHOOSEN");
+    }
+    // let empty = ndarray::Array2::<f32>::zeros((0,0));
+    Ok(res)
+}
+
+pub fn as_2d_array(filename: &str, dataset: &str) -> ndarray::Array2::<f32> {
+    match get_data_array(filename, dataset) {
+        Ok(_v) => return _v,
+        Err(_e) => return ndarray::Array2::<f32>::zeros((0,0))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,13 @@ fn main() {
     // let res = hdf5_example2::run();
     // println!("{:?}", res);
 
-    let _res = datasets::get_dataset();
+    let _res = datasets::get_data_array("../../datasets/glove-100-angular.hdf5", "train").unwrap();
+    for x in _res.iter() {
+        println!("{}", x);
+    }
+    
+    let _res2 = datasets::as_2d_array("../../datasets/glove-100-angular.hdf5", "test");
+    let _res3 = datasets::as_2d_array("../../datasets/glove-100-angular.hdf5", "neighbors");
+    let _res4 = datasets::as_2d_array("../../datasets/glove-100-angular.hdf5", "distances");
     
 }
