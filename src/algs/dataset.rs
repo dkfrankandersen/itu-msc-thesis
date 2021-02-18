0fcb2b661e9fdfe252577d373e54ce3fc7719dd2
fand <fand@itu.dk>
2021-02-18 12:42:13 +0100

    Removing commented code

--- src/algs/dataset.rs
+++ src/algs/dataset.rs
@@ -15,15 +15,15 @@ impl Dataset {
         }
     }
 
-    pub fn get_dataset_f64(&self, dataset: &str) -> Array2::<f64> {
+    pub fn get(&self, dataset: &str) -> Array2::<f64> {
         return (self.hdf5_file).dataset(dataset).unwrap().read_2d::<f64>().unwrap();
     }
 
-    pub fn get_dataset_usize(&self, dataset: &str) -> Array2::<usize> {
+    pub fn get_as_usize(&self, dataset: &str) -> Array2::<usize> {
         return (self.hdf5_file).dataset(dataset).unwrap().read_2d::<usize>().unwrap();
     }
 
-    pub fn get_dataset_normalize(&self, dataset: &str) -> Array2::<f64> {
+    pub fn get_as_normalize(&self, dataset: &str) -> Array2::<f64> {
         let dataset = (self.hdf5_file).dataset(dataset).unwrap().read_2d::<f64>().unwrap();
         let mut ds_new: Array2<f64> = dataset.clone();
         for (idx_row, p) in dataset.outer_iter().enumerate() {
@@ -35,7 +35,7 @@ impl Dataset {
         return ds_new;
     }
 
-    fn normalize_all(&self, dataset: Array2<f64>) -> Array2::<f64> {
+    pub fn normalize_all(&self, dataset: Array2<f64>) -> Array2::<f64> {
         let mut ds_new: Array2<f64> = dataset.clone();
         for (idx_row, p) in dataset.outer_iter().enumerate() {
             let magnitude = p.dot(&p).sqrt();        
@@ -45,32 +45,9 @@ impl Dataset {
         }
         return ds_new;
     }
-}
-
-pub fn get_dataset(filename: &str) -> hdf5::File {
-    return hdf5::File::open(filename).unwrap();
-}
-
-pub fn get_dataset_f64(file: &hdf5::File, dataset: &str) -> Array2::<f64> {
-    return (file).dataset(dataset).unwrap().read_2d::<f64>().unwrap();
-}
-
-pub fn get_dataset_usize(file: &hdf5::File, dataset: &str) -> Array2::<usize> {
-    return (file).dataset(dataset).unwrap().read_2d::<usize>().unwrap();
-}
-
-pub fn normalize_vector(p: &ArrayView1::<f64>)-> Array1::<f64>  {
-    let magnitude = p.dot(p).sqrt();
-    return p.map(|e| e/magnitude);
-}
-
-pub fn normalize_all(dataset: Array2<f64>) -> Array2::<f64> {
-    let mut ds_new: Array2<f64> = dataset.clone();
-    for (idx_row, p) in dataset.outer_iter().enumerate() {
-        let magnitude = p.dot(&p).sqrt();        
-        for (idx_col, val) in p.iter().enumerate() {
-            ds_new[[idx_row, idx_col]] = val/magnitude;
-        }            
+    
+    pub fn normalize_vector(p: &ArrayView1::<f64>)-> Array1::<f64>  {
+        let magnitude = p.dot(p).sqrt();
+        return p.map(|e| e/magnitude);
     }
-    return ds_new;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,18 @@ extern crate ndarray;
 extern crate hdf5;
 use std::time::{Instant};
 mod algs;
-use algs::{dataset,pq,bruteforce};
+use algs::{bruteforce};
 use algs::dataset::Dataset;
 
 fn main() {
-    let _e = hdf5::silence_errors();
-    let _filename = "datasets/glove-100-angular.hdf5";
-    // let file = &dataset::get_dataset(_filename);
+    let filename = "datasets/glove-100-angular.hdf5";
 
-    let ds = Dataset::new(_filename);
+    let ds = Dataset::new(filename);
 
-    let ds_train_norm = ds.get_dataset_normalize("train");
-    let ds_test_norm = ds.get_dataset_normalize("test");
-    let ds_distance_norm = ds.get_dataset_normalize("distances");
-    let ds_neighbors = ds.get_dataset_usize("neighbors");
-    
-    // let ds_train = dataset::get_dataset_f64(file, "train");
-    // let ds_train_norm = dataset::normalize_all(ds_train);
-    
-    // let ds_test = dataset::get_dataset_f64(file, "test");
-    // let ds_test_norm = &dataset::normalize_all(ds_test);
-
-    // let ds_distance = dataset::get_dataset_f64(file, "distances");
-    // let ds_distance_norm = dataset::normalize_all(ds_distance);
-
-    // let ds_neighbors = dataset::get_dataset_usize(file, "neighbors");
+    let ds_train_norm = ds.get_as_normalize("train");
+    let ds_test_norm = ds.get_as_normalize("test");
+    let ds_distance_norm = ds.get_as_normalize("distances");
+    let ds_neighbors = ds.get_as_usize("neighbors");
 
     let time_start = Instant::now();
 
@@ -40,9 +27,7 @@ fn main() {
         break
     }
     println!("end ds_neighbors: ");
-        
-
-
+    
     // bruteforce_search
     println!("bruteforce_search started at {:?}", time_start);
     for (i,p) in ds_test_norm.outer_iter().enumerate() {
@@ -51,7 +36,6 @@ fn main() {
         println!("{:?}", res1);
         break;
     }
-
 
     let time_finish = Instant::now();
     println!("Duration: {:?}", time_finish.duration_since(time_start));
