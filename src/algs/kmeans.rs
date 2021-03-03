1ec3dd435a85eb0353b6a60f2dcb5808562b16a5
Frank Andersen <mail@frankandersen.dk>
2021-03-03 14:40:47 +0100

    WIP k-means, with better print

--- src/algs/kmeans.rs
+++ src/algs/kmeans.rs
@@ -1,75 +1,128 @@
-use ndarray::{Array2, ArrayView1, ArrayView2, s};
+use ndarray::{ArrayBase, Array, Array1, Array2, ArrayView1, ArrayView2, s};
 use crate::algs::distance;
 use crate::algs::pq;
 use std::collections::BinaryHeap;
 use rand::prelude::*;
 use std::collections::HashMap;
+use std::cmp::Ordering;
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Centroid {
+    pub point: Array1::<f64>,
+    pub childern: Vec::<usize>
+}
+
+impl Centroid {
+    fn new(point: Array1::<f64>) -> Centroid {
+        Centroid {
+            point: point,
+            childern: Vec::<usize>::new()
+        }
+    }
+}
+
+// impl Eq for Centroid {}
+
+// impl PartialOrd for Centroid {
+//     fn partial_cmp(&self, other: &Centroid) -> Option<Ordering> {
+//         self.point.partial_cmp(&other.point)
+//     }
+// }
+
+// impl Ord for Centroid {
+//     fn cmp(&self, other: &Centroid) -> Ordering {
+//         self.partial_cmp(other).unwrap()
+//     }
+// }
 
 pub fn query(p: &ArrayView1::<f64>, dataset: &ArrayView2::<f64>, result_count: u32) -> Vec<usize> {
 
     /*
-        Init
-        Assign
-        Update
-        Repeat until convergence or some iteration count
-
-        Repeat X times, select best based on cluster density
+        Repeat X times, select best based on cluster density {
+            Repeat until convergence or some iteration count {
+                Init
+                Assign
+                Update
+            }
+            Save result
+        }
+        
+        Pick best result        
     */
 
-    // 1. Init
-    let k = 10;
+    
+    let k = 2;
     let max_iterations = 100;
     let max_samples = 10;
     let n = &dataset.shape()[0]; // shape of rows, cols (vector dimension)
+
+
     let mut rng = thread_rng();
     let dist_uniform = rand::distributions::Uniform::new_inclusive(0, n);
     let mut init_k_sampled: Vec<usize> = vec![];
-    let mut codebook = HashMap::new();
-    let mut codebook_cluster = HashMap::new();
     
-    for _ in 0..k {
+    // 1. Init
+    let mut codebook = HashMap::new();
+    for i in 0..k {
         let rand_key = rng.sample(dist_uniform);
         init_k_sampled.push(rand_key);
-        codebook.insert(k, Vec::<usize>::new());
-        let centroid = dataset.slice(s![rand_key,..]);
-        codebook_cluster.insert(k, centroid);
+        let candidate = dataset.slice(s![rand_key,..]);
+        let new_centroid = Centroid::new(candidate.to_owned());
+        codebook.insert(i, new_centroid);
     }
 
     println!("Dataset lenght: {}", n);
-    println!("Init k-means with centroids: {:?}", init_k_sampled);
-    println!("Centroids: {:?}", codebook_cluster);
-
+    println!("Init k-means with centroids: {:?}\n", init_k_sampled);
+    print_codebook("Codebook after init", &codebook);
     
     // 2. Assign
-    println!("Let look for my favorit centroid!");
+    println!("Let's look for my favorit centroid!");
     for (idx, candidate) in dataset.outer_iter().enumerate() {
         let mut best_centroid = 0;
         let mut best_distance = f64::INFINITY;
-        for (&key,_) in codebook.iter() {
-            let centroid = &dataset.slice(s![key,..]);
-            let distance = distance::cosine_similarity(&centroid, &candidate);
+        for (&key, centroid) in codebook.iter() {
+            
+            let distance = distance::cosine_similarity(&(centroid.point).view(), &candidate);
             if best_distance > distance {
                 best_centroid = key;
                 best_distance = distance;
 
             }
         }
-        codebook.get_mut(&best_centroid).unwrap().push(idx);
-        // println!("Assign {} to centroid {} ", idx, best_centroid);
+        codebook.get_mut(&best_centroid).unwrap().childern.push(idx);
+        // println!("Assign datapoint {} to centroid C{} ", idx, best_centroid);
 
-        if idx > 100 {
+        if idx > 5 {
             break;
         }
     }
 
+    print_codebook("Codebook after assign", &codebook);
+
     // 3. Update
-    for (&key,vec) in codebook_cluster.iter() {
-        let local_minima = &dataset.slice(s![key,..]);
+    for (key, centroid) in codebook.iter_mut() {
+        
+            for i in 0..centroid.point.len() {
+                centroid.point[i] = 0.;
+            }
+
+            for child_key in centroid.childern.iter() {
+                let child_point = dataset.slice(s![*child_key,..]);
+                for (i, x) in child_point.iter().enumerate() {
+                    centroid.point[i] += x;
+                }
+            }
+
+            let dimensions = centroid.point.len() as f64;
+            for i in 0..centroid.point.len() {
+                centroid.point[i] = centroid.point[i]/dimensions;
+            }
     }
 
-    // 4. Repeat   
+    print_codebook("Codebook after update", &codebook);
 
-    println!("{:?}", codebook);
+    // 4. Repeat   
+    
 
     let mut best_n_candidates: Vec<usize> = Vec::new();
     // for _ in 0..result_count {
@@ -77,4 +130,12 @@ pub fn query(p: &ArrayView1::<f64>, dataset: &ArrayView2::<f64>, result_count: u
     //     best_n_candidates.push(idx.index);
     // }
     best_n_candidates
+}
+
+
+fn print_codebook(info: &str, codebook: &HashMap<i32, Centroid>) {
+    println!("{}", info);
+    for (key, centroid) in codebook.iter() {
+        println!("-> centroid C{:?} |  Childern: {:?} | point sum: {:?}", key, centroid.childern.len(), centroid.point.sum());
+    }
 }
