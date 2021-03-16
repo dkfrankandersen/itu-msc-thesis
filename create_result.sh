6561b186b9bf9f7ce241e59841981e7177c9d025
Frank Andersen <mail@frankandersen.dk>
2021-03-16 13:33:00 +0100

    refactor pq to data entry

--- create_result.sh
+++ create_result.sh
@@ -5,13 +5,9 @@ read ready
 
 if [ $ready = 'y' ]
 then
-
-    echo "Building with cargo, ignore dead code"
-    RUSTFLAGS="$RUSTFLAGS -A dead_code" cargo build
-
     for (( clusters=1; clusters<=1000; clusters=clusters*2 )) do
     echo "Running kmeans 10 $clusters 200 1"
-    cargo run --release cosine glove-100-angular kmeans 10 $clusters 200 1
+    RUSTFLAGS="$RUSTFLAGS -A dead_code" cargo run --release cosine glove-100-angular kmeans 10 $clusters 200 1
     done
 
 fi
