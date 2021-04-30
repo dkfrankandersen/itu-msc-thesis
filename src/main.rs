be66369d292c2e411c3edfa9f3c4ee0f615de5a8
Frank Andersen <mail@frankandersen.dk>
2021-04-30 10:47:35 +0200

    Changing main approch to simplify

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-extern crate ndarray;
-extern crate hdf5;
 use std::env;
 mod algs;
 use algs::dataset::Dataset;
 mod util;
-use util::{store_results_and_fix_attributes, hdf5_store_file, unzip_enclosed_text};
-
-struct RunParameters {
-    metric: String,
-    dataset: String,
-    algorithm: String,
-    additional: Vec<String>,
-}
-
-fn algo_definition(rp: &RunParameters) -> String {
-    let mut val: String = "".to_string();
-    for (i, v) in rp.additional.iter().enumerate() {
-        if i>0 { val.push_str("_"); }
-        val.push_str(v);
-    }
-    return format!("{}({}_{})", rp.algorithm, rp.metric, val);
-}
+use util::*;
+mod running;
 
 
 fn main() {
@@ -46,14 +29,17 @@ fn main() {
         return;
     }
     let para = if para_arguments.is_some() {para_arguments.unwrap().split_whitespace().map(|x| x.to_string()).collect()} else {Vec::<String>::new()};
-    let parameters = RunParameters{ 
+    let parameters = util::RunParameters{ 
                                     metric: args[1].to_string(), 
                                     dataset: args[2].to_string(),
                                     algorithm: args[3].to_string(),
                                     additional: para,
     };
-    
-    let algo_def = algo_definition(&parameters);
+
+    fn create_run_parameters(metric: String, dataset: String, algorithm: String, results: Vec<usize>, arguments: Vec::<String>, query_options: Vec::<String>) {
+        
+    } 
+
     let best_search_time = f64::INFINITY;
     let filename = format!("datasets/{}.hdf5",parameters.dataset);
     let ds = Dataset::new(&filename);
@@ -65,9 +51,11 @@ fn main() {
     // if verbose_print {
     //     ds.print_true_neighbors(0, 5, 10);
     // }
+
+
     
     let dataset = &ds_test_norm;
-    let (build_time, algo) = algs::get_fitted_algorithm(verbose_print, &parameters.algorithm, parameters.additional, &ds_train_norm.view());
+    let (build_time, algo) = algs::get_fitted_algorithm(verbose_print, &parameters.algorithm, parameters.additional.clone(), &ds_train_norm.view());
 
     let mut clusters_to_search: Vec::<usize> = Vec::<usize>::new();
     if para_query.is_some(){
@@ -78,11 +66,12 @@ fn main() {
         let mut results = Vec::<(f64, Vec<(usize, f64)>)>::new();
         if clusters_to_search.len() > 0 {
             for cluster_to_search in clusters_to_search.iter() {
-                let vec: Vec::<usize> = vec![*cluster_to_search];
+                let query_arguments: Vec::<usize> = vec![*cluster_to_search];
                 for (_, p) in dataset.outer_iter().enumerate() {
-                    let result = algs::run_individual_query(&algo, &p, &ds_train_norm.view(), *results_per_query, &vec);
+                    let result = algs::run_individual_query(&algo, &p, &ds_train_norm.view(), *results_per_query, &query_arguments);
                     results.push(result);
                 }
+                running::compute_timing_and_store(best_search_time, build_time, results.clone(), *results_per_query, dataset.nrows(), parameters.clone());
             }
         } else {
             let empty_args = &Vec::<usize>::new();
@@ -90,35 +79,10 @@ fn main() {
                 let result = algs::run_individual_query(&algo, &p, &ds_train_norm.view(), *results_per_query, empty_args);
                 results.push(result);
             }
+            running::compute_timing_and_store(best_search_time, build_time, results.clone(), *results_per_query, dataset.nrows(), parameters.clone());
         }
 
-        let mut total_time: f64 = 0.;
-        let mut total_candidates: usize = 0;
-        for (time, candidates) in results.iter() {
-            total_time += time;
-            total_candidates += candidates.len();
-        }
 
-        let search_time = total_time / dataset.nrows() as f64;
-        let avg_candidates = total_candidates as f64 / dataset.nrows() as f64;
-        let best_search_time = { if best_search_time < search_time { best_search_time } else { search_time }};
-
-        let attrs = hdf5_store_file::Attributes {
-            build_time: build_time,
-            index_size: 0.,
-            algo: parameters.algorithm.clone(),
-            dataset: parameters.dataset.clone(),
-
-            batch_mode: false,
-            best_search_time: best_search_time,
-            candidates: avg_candidates,
-            count: *results_per_query as u32,
-            distance: parameters.metric.clone(),
-            expect_extra: false,
-            name: algo_def.clone(),
-            run_count: 1
-        };
-        println!("Store results into HD5F file");
-        store_results_and_fix_attributes(results, attrs);
+        
     }
 }
