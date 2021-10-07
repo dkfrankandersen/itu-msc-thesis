a29143615bacad438a6a1504cf2f62c982f6e3b7
Frank Andersen <mail@frankandersen.dk>
2021-10-07 15:21:16 +0200

    beginning

--- run.sh
+++ run.sh
@@ -43,12 +43,12 @@ if [ $run_type = 't' ]
 then
     # cargo run --release angular $DATASET bruteforce [10]
     # cargo run --release angular $DATASET kmeans [10] [2000 10] [[16] [24] [32] [64] [128] [192] [256] [384] [512]]
-    cargo run --release angular $DATASET pq [10] [10 2000 250000 256 10] [[16 512] [24 1024] [64 2048] [72 3072] [88 4096] [128 8192] [128 12288] [192 16384] [256 32768] [384 73728]]
+    # cargo run --release angular $DATASET pq [10] [10 2000 250000 256 10] [[16 512] [24 1024] [64 2048] [72 3072] [88 4096] [128 8192] [128 12288] [192 16384] [256 32768] [384 73728]]
 
 
     # cargo run --release angular $DATASET kmeans [10] [50 100] [[1] [2] [3] [4] [5] [10] [30] [50]]
     # cargo run --release angular $DATASET pq [10] [5 500 250000 256 100] [[1 30] [1 60] [1 120] [1 150] [1 500] [5 30] [5 60] [5 120] [5 150] [10 500] [10 30] [10 60] [10 120] [10 150] [10 500]]
-    # cargo run --release angular $DATASET scann [10] [10 2000 250000 256 10 0.2] [[1 32]]
+    cargo run --release angular $DATASET scann [10] [10 2000 250000 256 10 0.2] [[1 32]]
 
 elif [ $run_type = 'f' ]
 then
